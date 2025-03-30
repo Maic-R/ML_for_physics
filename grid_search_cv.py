b51d90907c903db9f2546e2783aa392546a7c67d
Michele Rignanese <michelerignanese17@gmail.com>
2025-03-30 22:03:56 +0200

    EHN: features plot + stable DNN

--- grid_search_cv.py
+++ grid_search_cv.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import json
 
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
@@ -9,15 +8,15 @@ from sklearn.utils import resample
 def grid_search() -> dict:
 
     search_space = {
-        'n_estimators': np.arange(100, 1000, 50, int),  # number of trees in the forest
-        'max_depth': np.arange(2, 60, 2, int),          # depth of each tree
-        'min_samples_split': np.arange(2, 50, 2, int),  # minimum number of samples to split an internal node
-        'min_samples_leaf': np.arange(1, 20, 2, int)    # 
+        'n_estimators': np.arange(150, 500, 50, int),   # number of trees in the forest
+        'max_depth': np.arange(1, 30, 10, int),          # depth of each tree
+        'min_samples_split': np.arange(1, 30, 10, int),  # minimum number of samples to split an internal node
+        'min_samples_leaf': np.arange(1, 20, 5, int)    # 
     }
 
     rnd_forest = RandomForestClassifier(random_state=42)
 
-    grid_search = GridSearchCV(estimator=rnd_forest, param_grid=search_space, cv=5, n_jobs=-1)
+    grid_search = GridSearchCV(estimator=rnd_forest, param_grid=search_space, cv=5, n_jobs=-1, verbose=1)
 
     # load data for grid search
     ljet_data = pd.read_csv('jet_data/ljet_train.csv')
@@ -26,9 +25,10 @@ def grid_search() -> dict:
 
     jet_data = pd.concat([ljet_data, cjet_data, bjet_data], axis=0).reset_index(drop=True)
 
-    X = jet_data.columns.drop('mc_flavour')
-    y = jet_data['mc_flavour']
+    features_for_training = jet_data.columns.drop(['mc_flavour', 'PT']) # use all the features except PT. Remove label column
 
+    X = jet_data[features_for_training] # input for random forest are data of the selected features
+    y = jet_data['mc_flavour'] # output: label of the jet
 
     X_train, _, y_train, _ = train_test_split(X, y, train_size=0.8, random_state=42, stratify=y)
 
@@ -44,5 +44,4 @@ def grid_search() -> dict:
 if __name__ == '__main__':
     opt_hyperparameters = grid_search()
 
-    with open('grid_parameters.json', 'w') as f:
-        json.dump(opt_hyperparameters, f, indent=4)
+    print('Optimized hyperparamters: ', opt_hyperparameters)
